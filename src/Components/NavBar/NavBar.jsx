7b952231099974dadb268f3ccf29692e9bef0474
Ayo-JB-CODE <ayotunde8@gmail.com>
2024-08-20 00:58:55 +0100

    barbshoe3

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styles from './NavBar.module.css';
 import cart_icon from '../../assets/cart_icon.png';
 import user_icon from '../../assets/user-icon.svg';
@@ -7,6 +7,7 @@ import { ShopContext } from '../../Context/ShopContext';
 
 
 const NavBar = () => {
+    const [menu, setMenu] = useState("home")
     const { getTotalCartItems } = useContext(ShopContext);
     return (
         <div className={styles.nav_bar}>
@@ -14,17 +15,17 @@ const NavBar = () => {
                 <Link to='/'>BARB-STORE</Link>
             </div>
             <div className={styles.nav_menus}>
-                <Link to='/'>Home</Link>
-                <Link to="/Shop">Shop</Link>
-                <Link to="/Contact">Contact</Link>
+                <div><Link onClick={() => { setMenu("home") }} to='/'>Home</Link>{menu === "home" ? <hr /> : <></>}</div>
+                <div><Link onClick={() => { setMenu("shop") }} to="/Shop">Shop</Link>{menu === "shop" ? <hr /> : <></>}</div>
+                <div><Link onClick={() => { setMenu("contact") }} to="/Contact">Contact</Link>{menu === "contact" ? <hr /> : <></>}</div>
             </div>
             <div className={styles.login_n_cart}>
                 <div className={styles.nav_login}>
                     <img src={user_icon} alt="user_icon" />
-                    <Link to="/Login">Login</Link>
+                    <div><Link onClick={() => { setMenu("login") }} to="/Login">Login</Link>{menu === "login" ? <hr /> : <></>}</div>
                 </div>
                 <div className={styles.cart_icon}>
-                    <Link to="/Cart"><img src={cart_icon} alt="cart_icon" /></Link>
+                    <div><Link onClick={() => { setMenu("cart") }} to="/Cart"><img src={cart_icon} alt="cart_icon" /></Link>{menu === "cart" ? <hr /> : <></>}</div>
                     <div className={styles.cart_count}>{getTotalCartItems()}</div>
                 </div>
 
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './CSS/Contact.css'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className='contact_container'>
+            <h1>OUR COMPLETE CONTACT PAGE IS COMING SOON</h1>
+
+            <div>
+                <h2>In the mean time contact us on</h2>
+                <p><span>Mobile: </span> 08089052581</p>
+                <p><span>Email: </span> ayotunde8@gmail.com</p>
+            </div>
+        </div>
     )
 }
 
