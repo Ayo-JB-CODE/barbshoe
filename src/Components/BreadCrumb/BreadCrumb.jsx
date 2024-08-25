4a709210bbe04acc397076d27350de4d5df8ffe2
Ayo-JB-CODE <ayotunde8@gmail.com>
2024-08-25 22:22:35 +0100

    mobile responsiveness added

--- src/Components/BreadCrumb/BreadCrumb.jsx
+++ src/Components/BreadCrumb/BreadCrumb.jsx
@@ -5,16 +5,9 @@ const BreadCrumb = (props) => {
     const { product } = props;
     return (
         <div className={styles.breadcrumb_container}>
-            HOME <img src={arrow_icon} alt="" />  SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.title}
+            HOME <img src={arrow_icon} alt="" />  SHOP <img src={arrow_icon} alt="" /> {product.title}
         </div>
     )
 }
 
 export default BreadCrumb;
-
-{/* <NavBar />
-            <div className={styles.breadcrumb_container}>
-                Home <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> Title <img src={arrow_icon} alt="" /> Name
-            </div>
-            <ItemSelected />
-            <Footer /> */}
